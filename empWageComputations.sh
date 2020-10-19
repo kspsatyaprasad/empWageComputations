bd24eb13434f39596c20857a21ac0a3931f0ebf0
kspsatyaprasad <koppulasatyaprasad@gmail.com>
2020-10-19 22:39:13 +0530

    added parttime employee wage

--- empWageComputations.sh
+++ empWageComputations.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 EMP_RATE_PER_HR=20;
-full_day_empHrs=8;
-dailyWage=$(( EMP_RATE_PER_HR*full_day_empHrs ))
-echo daily employee wage for fullday hour is $dailyWage
+full_time_empHrs=8;
+part_time_empHrs=4;
+full_time_dailyWage=$(( EMP_RATE_PER_HR*full_time_empHrs ))
+part_time_dailyWage=$(( EMP_RATE_PER_HR*part_time_empHrs ))
+echo daily employee wage for full time hour is $full_time_dailyWage
+echo daily employee wage for part time hour is $part_time_dailyWage
