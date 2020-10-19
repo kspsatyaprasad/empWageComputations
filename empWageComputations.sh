26149981481faf92780ba55ad57599803d721017
kspsatyaprasad <koppulasatyaprasad@gmail.com>
2020-10-19 22:53:29 +0530

    added wages calculation till condition of totalworkinghours or days is reached for a month

--- empWageComputations.sh
+++ empWageComputations.sh
@@ -4,7 +4,9 @@ IS_PART_TIME=1;
 IS_FULL_TIME=2;
 monthlywage=0;
 totalworkingDays=0;
+totalEmpHr=0;
 NUM_WORKING_DAYS=20;
+MAX_HRS_IN_MONTH=100;
 empCheck=$(( RANDOM%3 ));
 case $empCheck in
 $IS_FULL_TIME)
@@ -17,11 +19,14 @@ empHrs=4
 empHrs=0
 ;;
 esac
-while [[ $totalworkingDays -lt $NUM_WORKING_DAYS ]]
+while [[ $totalEmpHrs -lt $MAX_HRS_IN_MONTH &&
+          $totalworkingDays -lt $NUM_WORKING_DAYS ]]
 do
 ((totalworkingDays++))
+totalEmpHrs=$(( totalEmpHrs+empHrs ))
 dailyWage=$(( $empHrs*$EMP_RATE_PER_HR ))
 monthlywage=$(( monthlywage+dailyWage ))
 done
 echo daily wage of an employee is $dailyWage
 echo monthlywage of an employee is $monthlywage
+
