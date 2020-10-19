b2b252e891bea0058aebfbd9208e36b3cb2c3275
kspsatyaprasad <koppulasatyaprasad@gmail.com>
2020-10-19 22:42:49 +0530

    added solution using switch case statement

--- empWageComputations.sh
+++ empWageComputations.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 EMP_RATE_PER_HR=20;
-full_time_empHrs=8;
-part_time_empHrs=4;
-full_time_dailyWage=$(( EMP_RATE_PER_HR*full_time_empHrs ))
-part_time_dailyWage=$(( EMP_RATE_PER_HR*part_time_empHrs ))
-echo daily employee wage for full time hour is $full_time_dailyWage
-echo daily employee wage for part time hour is $part_time_dailyWage
+IS_PART_TIME=1;
+IS_FULL_TIME=2;
+empCheck=$(( RANDOM%3 ));
+case $empCheck in
+$IS_FULL_TIME)
+empHrs=8
+;;
+$IS_PART_TIME)
+empHrs=4
+;;
+*)
+empHrs=0
+;;
+esac
+dailyWage=$(( $empHrs*$EMP_RATE_PER_HR ))
+echo daily wage of an employee is $dailyWage
