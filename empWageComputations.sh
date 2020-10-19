871d6ea2fc196ddd3492233906babceebec3f42a
kspsatyaprasad <koppulasatyaprasad@gmail.com>
2020-10-19 22:36:01 +0530

    added daily employee wage

--- empWageComputations.sh
+++ empWageComputations.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-empCheck=$((RANDOM%2));
-if [[ empCheck -eq 1 ]]
-then
-echo employee is present
-else
-echo employee is absent
-fi
+EMP_RATE_PER_HR=20;
+full_day_empHrs=8;
+dailyWage=$(( EMP_RATE_PER_HR*full_day_empHrs ))
+echo daily employee wage for fullday hour is $dailyWage
