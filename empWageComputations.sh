90f9f7e71c5d5883eb346419ccc4e2cf44ba7cc3
kspsatyaprasad <koppulasatyaprasad@gmail.com>
2020-10-19 22:47:15 +0530

    added monthly wages calculation

--- empWageComputations.sh
+++ empWageComputations.sh
@@ -2,6 +2,9 @@
 EMP_RATE_PER_HR=20;
 IS_PART_TIME=1;
 IS_FULL_TIME=2;
+monthlywage=0;
+totalworkingDays=0;
+NUM_WORKING_DAYS=20;
 empCheck=$(( RANDOM%3 ));
 case $empCheck in
 $IS_FULL_TIME)
@@ -14,5 +17,11 @@ empHrs=4
 empHrs=0
 ;;
 esac
+while [[ $totalworkingDays -lt $NUM_WORKING_DAYS ]]
+do
+((totalworkingDays++))
 dailyWage=$(( $empHrs*$EMP_RATE_PER_HR ))
+monthlywage=$(( monthlywage+dailyWage ))
+done
 echo daily wage of an employee is $dailyWage
+echo monthlywage of an employee is $monthlywage
